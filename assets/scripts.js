8b1d663a3b9b65e368b0f9066af31689f9bfaa4e
raphael <Raphael Godoi>
2020-07-31 12:33:26 -0300

    Validação das posições

--- assets/scripts.js
+++ assets/scripts.js
@@ -35,21 +35,67 @@ var tabuleiro = {
         }
         return true;
     },
+    validarPosicoes(px){
+        if(px == 'XXX' || px == 'OOO'){
+            return true
+        }
+    },
+    validarPosicoesHorizontais(posicoes){
+
+        l1 = posicoes[0].textContent + posicoes[1].textContent + posicoes[2].textContent
+        l2 = posicoes[3].textContent + posicoes[4].textContent + posicoes[5].textContent 
+        l3 = posicoes[6].textContent + posicoes[7].textContent + posicoes[8].textContent
+       
+        if(tabuleiro.validarPosicoes(l1)){
+            return true
+        } else if(tabuleiro.validarPosicoes(l2)){
+            return true
+        } else if (tabuleiro.validarPosicoes(l3)){
+            return true
+        }
+
+        return false
+    },
+    validarPosicoeVerticais(posicoes){
+
+        v1 = posicoes[0].textContent + posicoes[3].textContent + posicoes[6].textContent
+        v2 = posicoes[1].textContent + posicoes[4].textContent + posicoes[7].textContent 
+        v3 = posicoes[2].textContent + posicoes[5].textContent + posicoes[8].textContent
+       
+        if(tabuleiro.validarPosicoes(v1)){
+            return true
+        } else if(tabuleiro.validarPosicoes(v2)){
+            return true
+        } else if (tabuleiro.validarPosicoes(v3)){
+            return true
+        }
+
+        return false
+
+    },
+    validarPosicoesDiagonais(posicoes){
+
+        d1 = posicoes[0].textContent + posicoes[4].textContent + posicoes[8].textContent
+        d2 = posicoes[2].textContent + posicoes[4].textContent + posicoes[6].textContent 
+       
+       
+        if(tabuleiro.validarPosicoes(d1)){
+            return true
+        } else if(tabuleiro.validarPosicoes(d2)){
+            return true
+        } 
+
+        return false
+
+    },
     validarJogada(){
         
-        var posicoes = document.querySelectorAll(".posicao");
-
-        if(posicoes[0].textContent == posicoes[1].textContent && posicoes[1].textContent == posicoes[2].textContent){
-            return true
-        }
-
-        if(posicoes[3].textContent == posicoes[4].textContent && posicoes[4].textContent == posicoes[5].textContent){
-            return true
-        }
-
-        if(posicoes[6].textContent == posicoes[7].textContent && posicoes[7].textContent == posicoes[8].textContent){
-            return true
-        }
+        const posicoes = Array.from(document.querySelectorAll(".posicao"))
+        
+        console.log(tabuleiro.validarPosicoesHorizontais(posicoes))
+        console.log(tabuleiro.validarPosicoeVerticais(posicoes))
+        console.log(tabuleiro.validarPosicoesDiagonais(posicoes))
+        
 
     },
     reiniciarPartida(){
